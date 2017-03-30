0b3098004baa09da6d4776fb210ab19ad0100b18
Aaron English <Aaron.H.English@gmail.com>
2017-03-29 21:10:51 -0500

    Modified Procfile, Modified index.js, Modified package.json, added .env

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// load modules
-const http: require('http');
+// load module
+const http = require('http');
 
 //set port number
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 function handleRequest(res,res) {
 	res.end('<h1>Welcome to my webpage. Kick back, relax, stay a while.</h1>');
@@ -11,6 +11,6 @@ function handleRequest(res,res) {
 const server = http.createServer(handleRequest);
 
 //Start up our server
-server.listen(PORT, function(){
-	console.log("Server listening on: http://localhost:%s", PORT);
-} 
+server.listen(PORT, ()=>{
+	console.log("Server is listening on port ${PORT}`);
+});
