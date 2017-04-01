d7a52100c67cf89a16d4155b186b441a3f2c2b7a
Aaron English <Aaron.H.English@gmail.com>
2017-03-31 22:11:46 -0500

    Adds some notes

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var http = require('http');
-
+// var vs let vs const
+// var -- classic javascript way of declaring variable
+// const -- variable not supposed to change
+// let -- variable can change
+const http = require('http');
 const PORT=process.env.PORT || 8080;
 
 function handleRequest(req,resp){
-	resp.end('<h1>Welcome to my webpage</h1>');
+	console.log("hitting this function");
+	let msg = "Welcome to mord-haus, serving on " + PORT;
+	console.log(msg, PORT, false || "asdf");
+
+	// let a = "banana"
+	// `string`    `string ${a} - ${a} - ${a} - ${a}`
+	// "string"    "string" + a + " - " + a + " - "
+	// 'string'
+	resp.end(`<h1>${msg}</h1>`);
 }
 
-var server = http.createServer(handleRequest);
+const server = http.createServer(handleRequest);
 
 //start our server
 server.listen(PORT, function(){
